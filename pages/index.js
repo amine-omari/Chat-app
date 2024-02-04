3f0dd4421d3416bddcb9dde0981bf2c7f98195f0
amine alaoui <omari1amineco@gmail.com>
2024-02-04 11:18:48 +0100

    feat: add the onSbmit function in Auth page

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,31 @@
 import React, { useContext } from "react";
 
 import { Context } from "../context";
+import axios from "axios";
+import { useRouter } from "next/router";
 
 export default function Auth() {
-  const { setUsername, setSecret } = useContext(Context);
+  const { username, setUsername, secret, setSecret } = useContext(Context);
 
+  const router = useRouter();
+
+  function onSubmit(e) {
+    e.preventDefault();
+
+    if (username.lenght === 0 || secret.lenght === 0) return;
+
+    axios
+      .put(
+        "https://api.chatengine.io/users/",
+        { username, secret },
+        { headers: { "private-key": "8b2e3864-28ef-4387-a539-f2193978dac5" } }
+      )
+      .then((r) => router.push("/chats"));
+  }
   return (
     <div className="background">
       <div className="auth-container">
-        <form className="auth-form" onSubmit={(e) => e.preventDefault()}>
+        <form className="auth-form" onSubmit={(e) => onSubmit(e)}>
           <div className="auth-title">Nextjs Chat</div>
           <div className="input-container">
             <input
