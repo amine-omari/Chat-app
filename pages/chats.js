0958346342c9274325d8cab62c6442968f93b965
amine alaoui <omari1amineco@gmail.com>
2024-02-04 11:34:13 +0100

    feat: add imports from chat engine

--- pages/chats.js
+++ pages/chats.js
@@ -1,4 +1,13 @@
+import dynamic from "next/dynamic";
 import React from "react";
+
+const ChatEngine = dynamic(() =>
+  import("react-chat-engine").then((module) => module.ChatEngine)
+);
+const MessageFormSocial = dynamic(() =>
+  import("react-chat-engine").then((module) => module.MessageFormSocial)
+);
+
 
 export default function Chats() {
   return <div className="background">chats</div>;
